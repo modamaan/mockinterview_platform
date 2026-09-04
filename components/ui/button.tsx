9359132642824097c0ec327b972f99eb70bfa060
Mohamed amaan <121436543+modamaan@users.noreply.github.com>
2026-09-04 21:05:58 +0530

    fix: components/ui/button.tsx
    Closes #14 — Update the frontend UI design for improved aesthetics and usability.

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -6,11 +6,11 @@ import { cn } from "@/lib/utils";
 
 // Define button variants using class-variance-authority
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-lg text-base font-semibold ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 uppercase tracking-wide",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-lg text-sm font-semibold ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 uppercase tracking-wide",
   {
     variants: {
       variant: {
-        default: "bg-white text-black border-slate-200 border-2 border-b-4 active:border-b-2 hover:text-slate-500 hover:bg-slate-100",
+        default: "bg-white text-black border-slate-200 border-2 border-b-4 active:border-b-2 hover:text-slate-600 hover:bg-slate-200",
         destructive: "bg-red-500 text-white hover:bg-red-600",
         outline: "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
@@ -27,10 +27,10 @@ const buttonVariants = cva(
         sidebarOutline: "bg-blue-100 text-blue-600 border-blue-300 border-2 hover:bg-blue-200 transition-none",
       },
       size: {
-        default: "h-12 px-5 py-3",
-        sm: "h-10 rounded-md px-4",
-        lg: "h-14 rounded-md px-10",
-        icon: "h-12 w-12",
+        default: "h-10 px-5 py-2",
+        sm: "h-9 rounded-md px-4",
+        lg: "h-12 rounded-md px-10",
+        icon: "h-10 w-10",
       },
     },
     defaultVariants: {
