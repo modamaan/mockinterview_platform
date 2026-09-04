026c9b48139656fb2e962eeee2213402c2b1b370
Mohamed amaan <121436543+modamaan@users.noreply.github.com>
2026-09-04 11:10:20 +0530

    security: auto-fix 1 finding(s) in app/dashboard/_components/PricingPlan.jsx
      - Potential secret detected: Base64 High Entropy String

--- app/dashboard/_components/PricingPlan.jsx
+++ app/dashboard/_components/PricingPlan.jsx
@@ -1,14 +1,16 @@
+```jsx
 export default[
     {
         link:'https://buy.stripe.com/test_9AQ4jI8fffk75igcMN',
         price:7.99,
-        priceId:'price_1PUNYmSISOkj08jR30L0UpyY',
+        priceId: process.env.PRICE_ID_MONTHLY,
         duration:'Monthly'
     },
     {
         link:'https://buy.stripe.com/test_eVabMa8fffk78us000',
         price:49.00,
-        priceId:'price_1PUNZQSISOkj08jRFQkKuzXi',
+        priceId: process.env.PRICE_ID_YEARLY,
         duration:'Yearly'
     }
 ]
+```
